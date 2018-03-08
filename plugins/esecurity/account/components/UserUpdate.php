aef6d5c8165421880f9d5b99300cd523cff914af
LogiMonde media <web@logimonde.com>
2018-03-08 15:20:10 -0500

    Update Plugin & theme

--- plugins/esecurity/account/components/UserUpdate.php
+++ plugins/esecurity/account/components/UserUpdate.php
@@ -10,31 +10,7 @@ use Esecurity\Projects\Models\Order;
 class UserUpdate extends ComponentBase
 {
 
-
-    public $maxSize;
-    public $previewWidth;
-    public $previewHeight;
-    public $previewMode;
-    public $previewFluid;
-    public $placeholderText;
-
     public $user;
-
-    /**
-     * Supported file types.
-     * @var array
-     */
-    public $fileTypes;
-
-    /**
-     * @var bool Has the model been bound.
-     */
-    protected $isBound = false;
-
-    /**
-     * @var bool Is the related attribute a "many" type.
-     */
-    public $isMulti = false;
 
     public function componentDetails()
     {
@@ -47,53 +23,6 @@ class UserUpdate extends ComponentBase
     public function defineProperties()
     {
         return [
-            'placeholderText' => [
-                'title' => 'Placeholder text',
-                'description' => 'Wording to display when no image is uploaded',
-                'default' => 'Click or drag images to upload',
-                'type' => 'string',
-            ],
-            'maxSize' => [
-                'title' => 'Max file size (MB)',
-                'description' => 'The maximum file size that can be uploaded in megabytes.',
-                'default' => '5',
-                'type' => 'string',
-            ],
-            'fileTypes' => [
-                'title' => 'Supported file types',
-                'description' => 'File extensions separated by commas (,) or star (*) to allow all types.',
-                'default' => '.gif,.jpg,.jpeg,.png',
-                'type' => 'string',
-            ],
-            'previewWidth' => [
-                'title' => 'Image preview width',
-                'description' => 'Enter an amount in pixels, eg: 100',
-                'default' => '120',
-                'type' => 'string',
-            ],
-            'previewHeight' => [
-                'title' => 'Image preview height',
-                'description' => 'Enter an amount in pixels, eg: 100',
-                'default' => '120',
-                'type' => 'string',
-            ],
-            'previewMode' => [
-                'title' => 'Image preview mode',
-                'description' => 'Thumb mode for the preview, eg: exact, portrait, landscape, auto or crop',
-                'default' => 'auto',
-                'type' => 'string',
-            ],
-            'previewFluid' => [
-                'title' => 'Fluid preview',
-                'description' => 'The image should expand to fit the size of its container',
-                'default' => 1,
-                'type' => 'checkbox',
-            ],
-            'deferredBinding' => [
-                'title' => 'Use deferred binding',
-                'description' => 'If checked the associated model must be saved for the upload to be bound.',
-                'type' => 'checkbox',
-            ],
             'pageNumber' => [
                 'title' => 'Page number',
                 'description' => 'This value is used to determine what page the user is on.',
@@ -142,12 +71,11 @@ class UserUpdate extends ComponentBase
 
     private function getOrders($page = null)
     {
-        $sort = isset($params['sort']) ? $params['sort'] : $this->property('sortOrder');
+
         $page = !is_null($page) ? $page : $this->property('pageNumber');
 
         $orders = Order::listOrders([
             'page' => $page,
-            'sort' => $sort,
             'perPage' => $this->property('itemsPerPage'),
             'user' => $this->user->id
         ]);
@@ -177,7 +105,7 @@ class UserUpdate extends ComponentBase
         $user = $this->model;
 
         $data = post();
-        $data['city_id'] = post('city');
+        $data['city'] = post('city');
         $data['state_id'] = post('state');
         $data['country_id'] = post('country');
         $user->fill($data);
@@ -196,64 +124,6 @@ class UserUpdate extends ComponentBase
 
         if ($redirectUrl = post('redirect', $redirectUrl))
             return Redirect::intended($redirectUrl);
-    }
-
-    public function getThumb($image = null)
-    {
-        if (!$image) {
-            $image = $this->getPopulated();
-        }
-
-        return $image->getThumb($this->previewWidth, $this->previewHeight, [
-            'extension' => 'png',
-            'mode' => $this->previewMode
-        ]);
-    }
-
-    public function getCssSize($addition = 0)
-    {
-        $width = $this->previewWidth != 'auto'
-            ? ($this->previewWidth + $addition) . 'px;'
-            : null;
-
-        $height = $this->previewHeight != 'auto'
-            ? ($this->previewHeight + $addition) . 'px;'
-            : null;
-
-        if ($this->previewFluid) {
-            $css = 'max-width: ' . ($width ?: 'none') . 'max-height: ' . ($height ?: 'none');
-        } else {
-            $css = 'width: ' . ($width ?: 'auto') . 'height: ' . ($height ?: 'auto');
-        }
-
-        return $css;
-    }
-
-    //
-    // AJAX
-    //
-
-    public function onRender()
-    {
-        if (!$this->isBound)
-            throw new ApplicationException('There is no model bound to the uploader!');
-
-        if ($populated = $this->property('populated')) {
-            $this->populated = $populated;
-        }
-    }
-
-    public function onUpdateImage()
-    {
-        $image = $this->getPopulated();
-
-        if (($deleteId = post('id')) && post('mode') == 'delete') {
-            if ($deleteImage = $image->find($deleteId)) {
-                $deleteImage->delete();
-            }
-        }
-
-        $this->page['image'] = $image;
     }
 
     /**
--- plugins/esecurity/projects/models/Order.php
+++ plugins/esecurity/projects/models/Order.php
@@ -39,4 +39,74 @@ class Order extends Model
     public $morphMany = [];
     public $attachOne = [];
     public $attachMany = [];
+
+    /**
+     * The attributes on which the post list can be ordered
+     * @var array
+     */
+    public static $allowedSortingOptions = array(
+        'created_at asc' => 'Created (ascending)',
+        'created_at desc' => 'Created (descending)',
+        'updated_at asc' => 'Updated (ascending)',
+        'updated_at desc' => 'Updated (descending)',
+        'random' => 'Random'
+    );
+
+    public function getItemsCountAttribute()
+    {
+        return $this->items()->count();
+    }
+
+    public function scopeListOrders($query, $options)
+    {
+        /*
+         * Default options
+         */
+        extract(array_merge([
+            'page' => 1,
+            'perPage' => 30,
+            'sort' => 'created_at',
+            'search' => '',
+            'user' => null,
+        ], $options));
+
+        $searchableFields = ['title', 'description'];
+
+        if ($user) {
+            $query->where('user_id', $user);
+        }
+
+        /*
+         * Sorting
+         */
+        if (!is_array($sort)) {
+            $sort = [$sort];
+        }
+
+        foreach ($sort as $_sort) {
+
+            if (in_array($_sort, array_keys(self::$allowedSortingOptions))) {
+                $parts = explode(' ', $_sort);
+                if (count($parts) < 2) {
+                    array_push($parts, 'desc');
+                }
+                list($sortField, $sortDirection) = $parts;
+                if ($sortField == 'random') {
+                    $sortField = DB::raw('RAND()');
+                }
+                $query->orderBy($sortField, $sortDirection);
+            }
+        }
+
+        /*
+         * Search
+         */
+        $search = trim($search);
+        if (strlen($search)) {
+            $query->searchWhere($search, $searchableFields);
+        }
+
+
+        return $query->paginate($perPage, $page);
+    }
 }
